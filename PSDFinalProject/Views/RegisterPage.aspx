8bf8993a3a4a362957311a708757ccb91e358e49
Andrew Nicholas <andrew.nic3@gmail.com>
2024-06-13 14:51:17 +0700

    register page validation

--- PSDFinalProject/Views/RegisterPage.aspx
+++ PSDFinalProject/Views/RegisterPage.aspx
@@ -8,7 +8,42 @@
 </head>
 <body>
     <form id="form1" runat="server">
+        <h1>
+            Register Page
+        </h1>
         <div>
+            <asp:Label ID="LabelUsername" runat="server" Text="Username"></asp:Label>
+            <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelEmail" runat="server" Text="Email"></asp:Label>
+            <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+        </div>
+        <div >
+            <asp:Label ID="LabelGender" runat="server" Text="Gender"></asp:Label>
+            <asp:RadioButton ID="RadioButtonMale" runat="server" Text="Male" GroupName="gender" />
+            <asp:RadioButton ID="RadioButtonFemale" runat="server" GroupName="gender" Text="Female" />
+        </div>
+        <div>
+            <asp:Label ID="LabelPassword" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="TextBoxPassword" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelConfirmPassword" runat="server" Text="ConfirmPassword"></asp:Label>
+            <asp:TextBox ID="TextBoxConfirmPassword" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelDOB" runat="server" Text="Date of Birth"></asp:Label>
+            <asp:Calendar ID="CalendarDOB" runat="server"></asp:Calendar>
+        </div>
+        <div>
+            <asp:Label ID="LabelError" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="ButtonRegister" runat="server" Text="Register" OnClick="ButtonRegister_Click" />
+        </div>
+        <div>
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">Already Have account ? click here</asp:LinkButton>
         </div>
     </form>
 </body>
--- PSDFinalProject/Views/RegisterPage.aspx.cs
+++ PSDFinalProject/Views/RegisterPage.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
 
 namespace PSDFinalProject.Views
 {
@@ -11,7 +13,82 @@ namespace PSDFinalProject.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            bool validation = false;
+        }
 
+        protected void ButtonRegister_Click(object sender, EventArgs e)
+        {
+            string username = TextBoxUsername.Text;
+            string email = TextBoxEmail.Text;
+            string gender = RadioButtonMale.Checked ? "Male" : RadioButtonFemale.Checked ? "Female" : string.Empty;
+            string password = TextBoxPassword.Text;
+            string confirmpassword = TextBoxConfirmPassword.Text;
+            DateTime dob = CalendarDOB.SelectedDate;
+            bool validation = true;
+
+            if(username.Length <= 5 || username.Length >= 15 || username == null || username == "")
+            {
+                LabelError.Text = "username Length must be between 5 and 15, must be unique, cannot be empty";
+                validation = false;
+            }
+            else if(email.EndsWith(".com") == false || email == null || email == "")
+            {
+                LabelError.Text = "email must end with .com and cannot be empty";
+                validation = false;
+            }
+            else if(gender == "")
+            {
+                LabelError.Text = "gender must be chosen and cannot be empty";
+                validation = false;
+            }
+            else if(IsAlphanumeric(password) == false)
+            {
+                LabelError.Text = "must be alpha numeric";
+                validation = false;
+            }
+            else if(password.Equals(confirmpassword) == false || confirmpassword == null || confirmpassword == "")
+            {
+                LabelError.Text = "confirm password must be the same with password and cannot be empty";
+                validation = false;
+            }
+            else if(dob == DateTime.MinValue)
+            {
+                LabelError.Text = "DOB cannot be empty";
+                validation = false;
+            }
+
+
+            Response.Redirect("LoginPage.aspx");
+        }
+
+        private bool IsAlphanumeric(string input)
+        {
+            int validNum = 0;
+            int validLetter = 0;
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    validNum = 1; 
+                }
+                if (char.IsLetter(c))
+                {
+                    validLetter = 1;
+                }
+            }
+            if(validNum == 1 && validLetter == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LoginPage.aspx");
         }
     }
 }
